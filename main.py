79bb477c5d0da23f27e248f80c82f4e65eab1bbd
SanjithaAngaru <sunzitha.angaru@gmail.com>
2025-10-01 17:46:02 -0500

    Update main.py
    have to change because is only accepted one

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ print("Welcome to ChatbotA01 Food Delivery! I'll be here to help you today!")
 name = input("What's your name? :")
 age = input(f"Awesome {name}! How old are you!? :")
 
+
+#receipt number variables
+for i in name:
+    name+=1
+
 #intro statement + name and age variables
 print(f"Wow {name}! I can't belive your already {age}! Time flies by fast, don't you think?!")
-print(f"Well, moving on {name}! I am here to help you for any means neccessary for any questions about this tutoring program!")
+print("Your receipt number is ____")
+print(f"Well, moving on {name}! I am here to help you for any means neccessary for any questions about your food delivery services and questions you need to ask!")
 
 while True:
     #asking user how it can help them 
-    user_select = input("What would you like to know about this tutoring program? \n*press 1 for about\n*press 2 for course options\n*press 3 for contact information \n*press 4 to leave ChatbotA01 \n Enter your answer here: ")
+    user_select = input("What would you like to know about this food delivery \n*press 1 for about\n*press 2 for questions \n*press 3 for the driver information contact \n*press 4 to leave ChatbotA01 \n Enter your answer here: ")
     if user_select == "1":
-        print("This tutoring program is about...")
+        print("Hello, it seems like you have pressed 1. ")
         True
     elif user_select == "2":
-        print("Courses that the academy provides id...")
+        option_two()
         True
     elif user_select == "3":
         print("More information")
@@ -26,3 +32,10 @@ while True:
         False
     else:
         print("Invalid Option, please pick from the 1-4")
+def option_two():
+    location = input("You have pressed option 2. \nPlease enter your your location: ")
+    receipt_number = input("Please re-enter your receipt number:")
+    if receipt_number not in 
+    
+    
+    
